beccf1b4ef1a70e884f03682661ab6370234a4d0
Brennen Bull <brennenbull@gmail.com>
2017-08-07 13:38:29 -0600

    post and get api calls

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,43 @@
 (function() {
   'use strict';
+  angular.module('app', [])
+  .component('board', {
+    controller: Controller,
+    templateUrl: '../app/app.template.html'
+  });
+  Controller.$inject = ['$http']
+  function Controller($http, $window){
+    const vm = this;
 
+    vm.$onInit = function () {
+      vm.clicked = false;
+      vm.posts = [];
+      $http({
+          method: 'GET',
+          url: 'https://stark-mesa-75466.herokuapp.com/classifieds/'
+      }).then(function(response){
+        vm.posts = response.data;
+      })
+    }
+
+    vm.newPost = function() {
+      $http({
+        method: 'POST',
+        url: 'https://stark-mesa-75466.herokuapp.com/classifieds/',
+        data: vm.post
+      }).then(function(res){
+        vm.posts.push(res.data);
+        delete vm.post;
+        vm.clicked = false;
+      })
+    };
+
+    vm.isClicked = function(){
+      if(vm.clicked){
+        vm.clicked = false
+      } else {
+        vm.clicked = true
+      }
+    }
+  }
 }());
